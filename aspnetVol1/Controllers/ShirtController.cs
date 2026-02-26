292c774480c33b04ed45d1125ba9fcd0936df4b1
michalszczepanski <michalszczepanski07@gmail.com>
2026-02-26 21:14:18 +0100

    routing with MapControllers

--- aspnetVol1/Controllers/ShirtController.cs
+++ aspnetVol1/Controllers/ShirtController.cs
@@ -3,34 +3,35 @@ using Microsoft.AspNetCore.Mvc;
 namespace aspnetVol1.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class ShirtController : ControllerBase
     {
         [HttpGet]
-        [Route("shirts")]
+        //[Route("shirts")]
         public string GetShirts()
         {
             return "Reading all shirts from the database";
         }
-        [HttpGet]
-        [Route("shirts/{id}")]
+        [HttpGet("{id}")]
+        //[Route("shirts/{id}")]
         public string GetShirt(int id)
         {
             return "Reading shirt " + id;
         }
         [HttpPost]
-        [Route("shirts")]
+        //[Route("shirts")]
         public string CreateShirt()
         {
             return "Creating a new shirt in the database";
         }
-        [HttpPut]
-        [Route("shirts/{id}")]
+        [HttpPut("{id}")]
+        //[Route("shirts/{id}")]
         public string UpdateShirt(int id)
         {
             return "Updating shirt " + id;
         }
-        [HttpDelete]
-        [Route("shirts/{id}")]
+        [HttpDelete("{id}")]
+        //[Route("shirts/{id}")]
         public string DeleteShirt(int id)
         {
             return "Deleting shirt " + id;
